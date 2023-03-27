0ba9da1fe4e4fffa151f91584a7be61778d4770f
Arantza Parra <A01782023@tec.mx>
2023-03-27 12:19:38 -0600

    Created eventos_dom json

---
+++ Desarrollo web/Front/js/eventos_dom.js
@@ -0,0 +1,5 @@
+//q:¿Qué es el DOM?
+//r:Es la representación de un documento HTML en forma de árbol, en el que cada nodo es un objeto que representa una parte del documento.
+//q:¿que es un nodo?
+//r:Es un objeto que representa una parte del documento.
+
