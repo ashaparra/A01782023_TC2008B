f2288a5c3d52e3c6359004ca76ae2e505caa39ae
Arantza Parra <A01782023@tec.mx>
2023-03-30 23:54:15 -0600

    Finsished excercises and added file to index

--- Desarrollo web/Front/js/eventos_dom.js
+++ Desarrollo web/Front/js/eventos_dom.js
@@ -1,5 +1,21 @@
 //q:¿Qué es el DOM?
-//r:Es la representación de un documento HTML en forma de árbol, en el que cada nodo es un objeto que representa una parte del documento.
-//q:¿que es un nodo?
-//r:Es un objeto que representa una parte del documento.
+//r:DOM es la abreviatura de Document Object Model, y es una interfaz de programación de aplicaciones que permite a los programadores acceder y manipular el contenido, estructura y estilo de un documento HTML.
+//q:En el contexto de DOM,¿qué es un nodo?
+//r:Un nodo es un objeto que representa un elemento del documento HTML. Por ejemplo, un nodo puede ser un elemento, un atributo, un texto, un comentario, etc.
+//q:¿Como se accede a un elemento del DOM?
+//r:Para acceder a un elemento del DOM, se utiliza el método getElementById, que recibe como parámetro el id del elemento que se quiere acceder.
+//Q:¿Cómo se modifica un elemento del DOM?
+//R:Para modificar un elemento del DOM, se utiliza el método innerHTML, que recibe como parámetro el nuevo contenido del elemento.
+//q:Cuál es la diferencia entre getElementby* y querySelector*?
+//r:La diferencia es que getElementby* devuelve un arreglo con todos los elementos que coincidan con el criterio de búsqueda, mientras que querySelector* devuelve el primer elemento que coincida con el criterio de búsqueda.
+//q:¿Qué es un evento en DOM?
+//r:Un evento es una acción que sucede en el navegador, como por ejemplo, hacer clic en un botón, escribir en un campo de texto, etc.
+//q:¿Qué es un listener en el DOM?
+//r:Un listener es una función que se ejecuta cuando sucede un evento.
+//q:¿Que es un callback?
+//r:Un callback es una función que se pasa como parámetro a otra función.
+
+
+
+
 
