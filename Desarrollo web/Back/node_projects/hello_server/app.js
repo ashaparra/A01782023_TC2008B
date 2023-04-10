c02267372748bb619834b5b933c6dc670ee15a4e
Arantza Parra <A01782023@tec.mx>
2023-04-10 12:56:53 -0600

    Created first node project

---
+++ Desarrollo web/Back/node_projects/hello_server/app.js
@@ -0,0 +1,8 @@
+import express from 'express'
+const app = express()
+app.get('/api/hello', (req, res) => {
+    res.send('Hello World!')
+    })
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!')
+    })
