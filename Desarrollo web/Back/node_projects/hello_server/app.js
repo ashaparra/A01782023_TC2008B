4b5d2a358a01dd8accb25891c5bc6b05db8d89a7
Arantza Parra <A01782023@tec.mx>
2023-04-11 14:37:34 -0600

    Update app.js

--- Desarrollo web/Back/node_projects/hello_server/app.js
+++ Desarrollo web/Back/node_projects/hello_server/app.js
@@ -1,8 +1,32 @@
 import express from 'express'
+import fs from 'fs'
+
 const app = express()
+//const port = 5000
+
+app.use(express.json())
+app.use(express.static('./public'))
+
+app.get('/', (req, res)=>
+{
+    fs.readFile('./public/html/inde.html', 'utf8', 
+    (err, html) => {
+        if(err)
+        {
+            res.status(500).send('There was an error: ' + err)
+            return 
+        }
+        
+        console.log("Sending page...")
+        res.send(html)
+        console.log("Page sent!")
+    })
+})
+
+
 app.get('/api/hello', (req, res) => {
     res.send('Hello World!')
     })
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!')
+app.listen(5000, () => {
+    console.log('Example app listening on port 5000!')
     })
